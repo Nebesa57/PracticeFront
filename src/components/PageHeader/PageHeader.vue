e3767177175d5c5d819f98b287e6c4bea90ecca3
klukv <86426111+klukv@users.noreply.github.com>
2022-07-03 14:33:36 +0300

    updateHeader

--- src/components/PageHeader/PageHeader.vue
+++ src/components/PageHeader/PageHeader.vue
@@ -4,21 +4,44 @@
             <ul class="nav nav-tabs">
                 <li class="nav-item">
                     <button
-                        type="button"
+                        :class="{ activeBtn: nameButton === 'materials' }"
                         class="nav-link"
+                        vs-value="materials"
+                        type="button"
                         aria-current="page"
-                        @click="$router.push({ name: 'Materials' })"
+                        @click="
+                            $router.push({ name: 'Materials' });
+                            buttonClick($event, 'materials');
+                        "
                     >
                         Материалы
                     </button>
                 </li>
                 <li class="nav-item">
-                    <button class="nav-link" @click="$router.push({ name: 'IndividualPlan' })">
+                    <button
+                        :class="{ activeBtn: nameButton === 'indivPlan' }"
+                        class="nav-link"
+                        vs-value="indivPlan"
+                        @click="
+                            $router.push({ name: 'IndividualPlan' });
+                            buttonClick($event, 'indivPlan');
+                        "
+                    >
                         Индивидуальный план
                     </button>
                 </li>
                 <li class="nav-item">
-                    <button class="nav-link" @click="$router.push({ name: 'Colleagues' })">Коллеги</button>
+                    <button
+                        :class="{ activeBtn: nameButton === 'colleagues' }"
+                        class="nav-link"
+                        vs-value="colleagues"
+                        @click="
+                            $router.push({ name: 'Colleagues' });
+                            buttonClick($event, 'colleagues');
+                        "
+                    >
+                        Коллеги
+                    </button>
                 </li>
                 <div class="navbar-brand">
                     <img src="../../assets/logo.svg" alt="logo" width="130" height="58" />
@@ -35,8 +58,14 @@ export default {
     name: 'PageHeader',
     data() {
         return {
+            nameButton: '',
             msg: 'PageHeader',
         };
+    },
+    methods: {
+        buttonClick(event, id) {
+            this.nameButton = id;
+        },
     },
 };
 </script>
